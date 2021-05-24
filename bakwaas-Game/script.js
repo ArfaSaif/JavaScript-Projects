1babca24879d52bc2fad95e3b3fe272357108cf6
Arfa Saif <48233453+ArfaSaif@users.noreply.github.com>
2021-05-23 22:24:34 -0400

    Update script.js

--- bakwaas-Game/script.js
+++ bakwaas-Game/script.js
@@ -1,19 +1,5 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.message').textContent = 'Correct Number';
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 14;
-
-// document.querySelector('.guess').value = 5;
-// console.log(document.querySelector('.guess').value);
-
-// document.querySelector('.check').addEventListener('click', function () {
-//   console.log(document.querySelector('.guess').value);
-// });
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
